026c09022da68fbb4925e08415f24c60929e5a20
Pedro Matos <pedroddematos@gmail.com>
2023-03-14 21:39:45 +0000

    ex_2.4

--- course-information/src/components/Content.js
+++ course-information/src/components/Content.js
@@ -1,6 +1,16 @@
 import Part from "./Part";
 
-const Content = ({ course }) =>
-  course.parts.map((part) => <Part key={part.id} part={part} />);
+const Total = ({ total }) => <p>total of 42 exercises: {total}</p>;
+
+const Content = ({ course, total }) => {
+  return (
+    <>
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <Total total={total} />
+    </>
+  );
+};
 
 export default Content;
--- course-information/src/components/Course.js
+++ course-information/src/components/Course.js
@@ -1,11 +1,11 @@
 import Content from "./Content";
 import Header from "./Header";
 
-const Course = ({ course }) => {
+const Course = ({ course , total }) => {
   return (
     <>
       <Header name={course.name} />
-      <Content course={course} />
+      <Content course={course} total={total}/>
     </>
   );
 };
