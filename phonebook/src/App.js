b940533456a09d3cc50703220444169bd179d09a
Pedro Matos <pedroddematos@gmail.com>
2023-03-15 14:51:42 +0000

    ex_2.9*

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
 
   const change_handler = (setState) => (event) => setState(event.target.value);
 
@@ -31,11 +32,21 @@ const App = () => {
     setNewName("");
     setNewNumber("");
   };
+  
+  const displayed_persons = newFilter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(newFilter.toLowerCase())
+      )
+    : persons;
+  console.log(displayed_persons);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter:{" "}
+      <input value={newFilter} onChange={change_handler(setNewFilter)} />
       <form onSubmit={add_to_persons}>
+        <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={change_handler(setNewName)} />
         </div>
@@ -48,7 +59,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {displayed_persons.map((person) => (
         <p key={person.id}>
           {person.name} ----- {person.number}
         </p>
