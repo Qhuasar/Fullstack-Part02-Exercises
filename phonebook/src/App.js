95ce417e404e7a0e74a3a7d99c9b00821cdd454e
Pedro Matos <pedroddematos@gmail.com>
2023-03-15 14:30:06 +0000

    ex_2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,13 +3,12 @@ import uniquid from "uniquid";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: uniquid() },
+    { name: "Arto Hellas", number: 98734521, id: uniquid() },
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const change_handler = (event) => {
-    setNewName(event.target.value);
-  };
+  const change_handler = (setState) => (event) => setState(event.target.value);
 
   const check_if_exists = () => {
     for (let i = 0; i < persons.length; i++) {
@@ -20,21 +19,29 @@ const App = () => {
     }
     return "not";
   };
-  const add_name = (event) => {
+
+  const add_to_persons = (event) => {
     event.preventDefault();
     if (check_if_exists() === "exists") {
       return;
     }
-    setPersons(persons.concat({ name: newName, id: uniquid() }));
+    setPersons(
+      persons.concat({ name: newName, number: newNumber, id: uniquid() })
+    );
     setNewName("");
+    setNewNumber("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={add_name}>
+      <form onSubmit={add_to_persons}>
         <div>
-          name: <input value={newName} onChange={change_handler} />
+          name: <input value={newName} onChange={change_handler(setNewName)} />
+        </div>
+        <div>
+          number:{" "}
+          <input value={newNumber} onChange={change_handler(setNewNumber)} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -42,7 +49,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.id}>{person.name}</p>
+        <p key={person.id}>
+          {person.name} ----- {person.number}
+        </p>
       ))}
       ...
     </div>
