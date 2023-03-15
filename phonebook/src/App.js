9140d85319319f5b2e890ecbe2943351eb4cb8df
Pedro Matos <pedroddematos@gmail.com>
2023-03-15 18:54:01 +0000

    ex_2.10

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,16 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import uniquid from "uniquid";
 import Form from "./From";
 import List from "./List";
 import Input from "./Input";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
@@ -44,7 +40,12 @@ const App = () => {
         person.name.toLowerCase().includes(newFilter.toLowerCase())
       )
     : persons;
-  console.log(displayed_persons);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   return (
     <div>
